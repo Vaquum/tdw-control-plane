0780c7ae7c577532d777281aea96e39c1640a3aa
Mikko Kotila <mailme@mikkokotila.com>
2025-04-26 20:31:30 +0300

    chore: Update definitions.py

--- tdw_control_plane/definitions.py
+++ tdw_control_plane/definitions.py
@@ -1,35 +1,51 @@
+# How to add a new asset?
+
+# 1. Add the asset to the assets folder
+# 2. Add the asset to the imports bel
+# 3. Create a new job for the asset
+# 4. Add the job to the assets list
+# 5. Add the job to the jobs list
+# 6. If applicable, add a schedule for the job and add it to the schedules list
+
 from dagster import Definitions, define_asset_job, schedule
 
 from .assets.daily_trades_to_tdw import insert_daily_binance_trades_to_tdw
 from .assets.monthly_trades_to_tdw import insert_monthly_binance_trades_to_tdw
 from .assets.create_tdw_database import create_tdw_database
 from .assets.create_binance_trades_table import create_binance_trades_table
+from .assets.create_binance_trades_monthly_summary import create_binance_trades_monthly_summary
+
+# Database Maintenance Jobs
 
 create_tdw_database_job = define_asset_job(
     name="create_tdw_database_job",
-    selection=["create_tdw_database"]
-)
+    selection=["create_tdw_database"])
 
 create_binance_trades_table_job = define_asset_job(
     name="create_binance_trades_table_job",
-    selection=["create_binance_trades_table"]
-)
+    selection=["create_binance_trades_table"])
+
+# Data Insertion Jobs
 
 insert_monthly_binance_trades_job = define_asset_job(
     name="insert_monthly_trades_to_tdw_job",
-    selection=["insert_monthly_binance_trades_to_tdw"]
-)
+    selection=["insert_monthly_binance_trades_to_tdw"])
 
 insert_daily_binance_trades_job = define_asset_job(
     name="insert_daily_trades_to_tdw_job",
-    selection=["insert_daily_binance_trades_to_tdw"]
-)
+    selection=["insert_daily_binance_trades_to_tdw"])
+
+# summary Table Creation Jobs
+
+create_binance_trades_monthly_summary_job = define_asset_job(
+    name="create_binance_trades_monthly_summary_job",
+    selection=["create_binance_trades_monthly_summary"])
 
 @schedule(
     job=insert_daily_binance_trades_job,
     cron_schedule="0 1 * * *",
-    execution_timezone="UTC"
-)
+    execution_timezone="UTC")
+
 def daily_pipeline_schedule():
     return {}
 
@@ -37,12 +53,13 @@ defs = Definitions(
     assets=[create_tdw_database,
             create_binance_trades_table,
             insert_monthly_binance_trades_to_tdw,
-            insert_daily_binance_trades_to_tdw],
+            insert_daily_binance_trades_to_tdw,
+            create_binance_trades_monthly_summary],
     
     schedules=[daily_pipeline_schedule],
     
     jobs=[create_tdw_database_job,
           create_binance_trades_table_job,
           insert_monthly_binance_trades_job,
-          insert_daily_binance_trades_job]
-)
+          insert_daily_binance_trades_job,
+          create_binance_trades_monthly_summary_job])
