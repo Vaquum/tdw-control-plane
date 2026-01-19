8352773cfc4f7965836b822eee92f26c8a914806
Jatin Thakur <jtkalga@gmail.com>
2026-01-19 20:58:21 +0530

    misc: fixed formatting

--- tdw_control_plane/assets/monthly_futures_agg_trades_to_tdw.py
+++ tdw_control_plane/assets/monthly_futures_agg_trades_to_tdw.py
@@ -5,9 +5,7 @@ from tdw_control_plane.utils.check_if_has_header import check_if_has_header
 
 from dagster import asset, AssetExecutionContext, MonthlyPartitionsDefinition
 from tdw_control_plane.utils.get_clickhouse_client import get_clickhouse_client
-from tdw_control_plane.utils.get_tdw_monthly_table_config import (
-    get_tdw_monthly_table_config,
-)
+from tdw_control_plane.utils.get_tdw_monthly_table_config import get_tdw_monthly_table_config
 from tdw_control_plane.utils.asset_insert_to_tdw import asset_insert_to_tdw
 
 # There are two things to do here:
@@ -20,29 +18,29 @@ from tdw_control_plane.utils.asset_insert_to_tdw import asset_insert_to_tdw
 ## CONFIG STARTS ##
 
 # Set the database to be used
-CLICKHOUSE_DATABASE = os.environ.get("CLICKHOUSE_DATABASE", "tdw")
+CLICKHOUSE_DATABASE = os.environ.get('CLICKHOUSE_DATABASE', 'tdw')
 
 # Set the table to be used
-CLICKHOUSE_TABLE = os.environ.get("CLICKHOUSE_TABLE", "binance_futures_agg_trades")
+CLICKHOUSE_TABLE = os.environ.get('CLICKHOUSE_TABLE', 'binance_futures_agg_trades')
 
 # This is left as it is (for config)
-ID_COL = f"{CLICKHOUSE_TABLE.replace('binance_', '')}_id"
+ID_COL = f'{CLICKHOUSE_TABLE.replace("binance_", "")}_id'
 
 # Set the starting month
-MONTHLY_PARTITIONS = MonthlyPartitionsDefinition(start_date="2020-01-01")
+MONTHLY_PARTITIONS = MonthlyPartitionsDefinition(start_date='2020-01-01')
 
 # Set the base url for the files to download
-BASE_URL = "https://data.binance.vision/data/futures/um/monthly/aggTrades/BTCUSDT/"
+BASE_URL = 'https://data.binance.vision/data/futures/um/monthly/aggTrades/BTCUSDT/'
 
 # Set the column names as per the data
 DATA_COLS = [
     ID_COL,
-    "price",
-    "quantity",
-    "first_trade_id",
-    "last_trade_id",
-    "timestamp",
-    "is_buyer_maker",
+    'price',
+    'quantity',
+    'first_trade_id',
+    'last_trade_id',
+    'timestamp',
+    'is_buyer_maker',
 ]
 
 ## CONFIG ENDS ##
@@ -53,11 +51,11 @@ client = get_clickhouse_client()
 
 
 @asset(
-    group_name=f"create_db_table_{CLICKHOUSE_DATABASE}_{CLICKHOUSE_TABLE}",
-    description=f"Creates the db table {CLICKHOUSE_DATABASE}.{CLICKHOUSE_TABLE}",
+    group_name=f'create_db_table_{CLICKHOUSE_DATABASE}_{CLICKHOUSE_TABLE}',
+    description=f'Creates the db table {CLICKHOUSE_DATABASE}.{CLICKHOUSE_TABLE}',
 )
 def create_binance_futures_agg_trades_table(context: AssetExecutionContext):
-    client.command(f"""
+    client.command(f'''
         CREATE TABLE {CLICKHOUSE_DATABASE}.{CLICKHOUSE_TABLE} (
             {ID_COL}        UInt64  CODEC(Delta(8), ZSTD(3)),
             price           Float64 CODEC(Delta, ZSTD(3)),
@@ -68,32 +66,32 @@ def create_binance_futures_agg_trades_table(context: AssetExecutionContext):
             is_buyer_maker  UInt8   CODEC(ZSTD(1)),
             datetime        DateTime CODEC(Delta, ZSTD(3))
         )
-        {get_tdw_monthly_table_config(ID_COL)}""")
+        {get_tdw_monthly_table_config(ID_COL)}''')
     context.log.info(
-        f"Created database table {CLICKHOUSE_DATABASE}.{CLICKHOUSE_TABLE}."
+        f'Created database table {CLICKHOUSE_DATABASE}.{CLICKHOUSE_TABLE}.'
     )
 
 
 @asset(
     partitions_def=MONTHLY_PARTITIONS,
-    group_name=f"insert_monthly_data",
-    description=f"Inserts monthly data into {CLICKHOUSE_DATABASE}.{CLICKHOUSE_TABLE}.",
+    group_name=f'insert_monthly_data',
+    description=f'Inserts monthly data into {CLICKHOUSE_DATABASE}.{CLICKHOUSE_TABLE}.',
 )
 def insert_monthly_binance_futures_agg_trades_to_tdw(context):
     partition_date_str = context.asset_partition_key_for_output()
-    date_parts = partition_date_str.split("-")
+    date_parts = partition_date_str.split('-')
     year, month = date_parts[0], date_parts[1]
 
-    file_url = f"BTCUSDT-aggTrades-{year}-{month}.zip"
+    file_url = f'BTCUSDT-aggTrades-{year}-{month}.zip'
     context.log.info(
-        f"Processing selected partition: {partition_date_str}, file: {file_url}"
+        f'Processing selected partition: {partition_date_str}, file: {file_url}'
     )
 
     full_url = BASE_URL + file_url
 
     data = binance_file_to_polars(full_url, has_header=check_if_has_header(full_url))
     data.columns = DATA_COLS
-    context.log.info(f"Completed reading {BASE_URL} into a DataFrame.")
+    context.log.info(f'Completed reading {BASE_URL} into a DataFrame.')
 
     asset_insert_to_tdw(
         data, client, context, file_url, CLICKHOUSE_DATABASE, CLICKHOUSE_TABLE
